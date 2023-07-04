bd9c4f24d5deaabf4e44d32cee51d03523a2fe98
Kevin Bönisch <keboen@web.de>
2023-07-04 15:10:25 +0200

    Added TEST to datasets

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,16 +1,16 @@
 import * as mongodb from "mongodb";
 import { Dataset } from "./../models/dataset";
- 
+
 export const collections: {
    datasets?: mongodb.Collection<Dataset>;
 } = {};
- 
+
 export async function connectToDatabase(uri: string) {
    const client = new mongodb.MongoClient(uri);
    await client.connect();
- 
+
    const db = client.db("BABoenisch");
- 
-   const datasetCollection = db.collection<Dataset>("rateable_test_datasets");
+
+   const datasetCollection = db.collection<Dataset>("rateable_test_datasets_TEST");
    collections.datasets = datasetCollection;
 }
